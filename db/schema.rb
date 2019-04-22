40d35109c5498b118aea74590b1faed4c254f5b6
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-22 16:43:39 -0700

    Generate model: user

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_234213) do
+ActiveRecord::Schema.define(version: 2019_04_22_234258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "works", force: :cascade do |t|
     t.string "category"
