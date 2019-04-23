e5c6c3e1c6b76d2b9120c668933a2fb6ccb5e7d8
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-22 19:07:50 -0700

    Add CRUD actions for works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,80 @@
 class WorksController < ApplicationController
+
+  def index
+    @works = Work.all.where(deleted: nil)
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+
+    successful = @work.save
+    if successful
+      redirect_to works_path
+    else
+      render :new, status: :bad_request
+    end
+  end
+
+  def show
+    work_id = params[:id]
+
+    @work = Work.find_by(id: work_id)
+
+    unless @work
+      head :not_found
+    end
+  end
+
+  def edit
+    work_id = params[:id]
+
+    @work = Work.find_by(id: work_id)
+
+    unless @work
+      head :not_found
+    end
+  end
+
+  def update
+    @work = Work.find_by(id: params[:id])
+
+    unless @work
+      head :not_found
+      return
+    end
+
+    if @work.update(work_)params
+      redirect_to work_path(@work)
+    else
+      render :edit, status: :bad_request
+    end
+  end
+
+  def destroy
+    work_id = params[:id]
+
+    work = Work.find_by(id: work_id)
+
+    unless work
+      head :not_found
+      return
+    end
+
+    work.deleted = !work.deleted
+
+    work.save
+
+    redirect_to works_path
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:title)
+  end
+    
 end
