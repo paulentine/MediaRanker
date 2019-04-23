79ceb1cf4614bf3786d97dbba257781d2392f812
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-23 15:25:58 -0700

    Add routes for login & logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
 
   resources :users
   resources :works
+  
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 
 end
