8aa428b057a1b03b2c823e1ef0fe3368cf3b3f47
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-24 15:21:53 -0700

    Add featured works to index

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,19 @@
 class Work < ApplicationRecord
   has_and_belongs_to_many :users
+
+  def self.featured
+    return Work.first
+  end
+
+  def self.top_ten_albums
+    return Work.where(category: "album").sample(10)
+  end
+
+  def self.top_ten_books
+    return Work.where(category: "book").sample(10)
+  end
+
+  def self.top_ten_movies
+    return Work.where(category: "movie").sample(10)
+  end
 end
