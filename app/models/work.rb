029241649351a1b3ee85b8f94bc4a59002ffa237
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-26 11:27:10 -0700

    Handle `deleted` work

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,10 +2,10 @@ class Work < ApplicationRecord
   has_and_belongs_to_many :users
 
   def self.featured
-    return Work.first
+    return Work.first.where(deleted: false)
   end
 
   def self.top_ten(category)
-    return Work.where(category: category).sample(10)
+    return Work.where(category: category, deleted: false).sample(10)
   end
 end
