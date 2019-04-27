7092161c532e032d824839bdc732fbaf7f2d9e77
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-26 17:27:18 -0700

    Add user controller action for index & show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    user_id = params[:id]
+    @user = User.find_by(id: user_id)
+  
+    unless @user
+      head :not_found
+    end
+  end
+
+
   def login_form
     @user = User.new
   end
