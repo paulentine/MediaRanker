a8c2a70472c19556c3fb24252380b4286f582107
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-26 19:23:37 -0700

    Add Featured & Top Ten methods

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,11 +3,21 @@ class Work < ApplicationRecord
 
   validates :title, presence: true, uniqueness: true
 
+  def self.not_deleted
+    return Work.where(deleted: false)
+  end
+
   def self.featured
-    return Work.first
+    return not_deleted.all.max_by { |work| work.users.count }
   end
 
   def self.top_ten(category)
-    return Work.where(category: category, deleted: false).sample(10)
+    category_works = not_deleted.where(category: category).sort_by { |work| work.users.count }
+    cat_works = category_works.reverse
+    if cat_works.length >= 10
+      return cat_works.first(10)
+    else
+      return cat_works
+    end
   end
 end
