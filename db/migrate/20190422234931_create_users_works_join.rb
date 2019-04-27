6443911b392f99bedfcf1dfda4aead3b230d5889
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-26 17:26:38 -0700

    Fix join table

--- db/migrate/20190422234931_create_users_works_join.rb
+++ db/migrate/20190422234931_create_users_works_join.rb
@@ -3,8 +3,8 @@
 class CreateUsersWorksJoin < ActiveRecord::Migration[5.2]
   def change
     create_table :users_works do |t|
-      t.belongs_to :users, index: :true
-      t.belongs_to :works, index: :true
+      t.belongs_to :user, index: :true
+      t.belongs_to :work, index: :true
     end
   end
 end
