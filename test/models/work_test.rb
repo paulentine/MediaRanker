1ed6b98c8d286daeaba233d8eed029ef51386043
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-29 15:11:21 -0700

    Test Work Model validation & relationships

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,71 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  before do
+    @work = works(:book)
+  end
 
-  it "must be valid" do
-    value(work).must_be :valid?
+  describe "Validation" do
+    it "Must be valid with good data" do
+      result = @work.valid?
+
+      expect(result).must_equal true
+    end
+
+    it "Raises an error when creating work without a title" do
+      work = Work.new(
+        title: nil,
+      )
+    end
+
+    it "Raises an error if title is not unique" do
+      # arrange
+      duplicate_title = @work.title
+
+      work = Work.new(
+        title: duplicate_title,
+      )
+
+      # act
+      result = work.valid?
+
+      # assert
+      expect(result).must_equal false
+      expect(work.errors.messages).must_include :title
+    end
+  end
+
+  describe "Relations" do
+    it "Has votes (through relship with users)" do
+      @work.users << users(:zelda)
+      expect(@work.users.length).must_equal 1
+
+      @work.users << users(:annie)
+      expect(@work.users.length).must_equal 2
+    end
+  end
+
+  describe "Featured" do
+    it "Returns the highest-voted work" do
+
+    end
+  end
+
+  describe "Top Ten" do
+    it "Returns top 10 highest-rated work for each category" do
+
+    end
+
+    it "Returns all work if the category has less than 10 works" do
+
+    end
+
+    it "Returns 10 works even if no vote has ever been casted" do
+    
+    end
+
+    it "Handles ties in number of votes" do
+
+    end
   end
 end
