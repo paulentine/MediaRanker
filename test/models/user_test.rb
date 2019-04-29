bbda034194df2f663ab3d2f4137457408321f186
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-29 15:11:05 -0700

    Test User Model validation & relationships

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,48 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  before do
+    @user = users(:zelda)
+  end
+  
+  describe "Validation" do
+    it "Must be valid with good data" do
+      expect(@user).must_be :valid?
+    end
 
-  it "must be valid" do
-    value(user).must_be :valid?
+    it "Raises an error when creating user without username" do
+      user = User.new(
+        username: nil,
+      )
+    end
+
+    it "Raises an error if username is not unique" do
+      # arrange
+      duplicate_name = @user.username
+
+      user = User.new(
+        username: duplicate_name,
+      )
+
+      # act
+      result = user.valid?
+
+      # assert
+      expect(result).must_equal false
+      expect(user.errors.messages).must_include :username
+    end
+  end
+
+  describe "Relations" do
+    it "Has votes (through relship with works)" do
+      @user.works << works(:album)
+      expect(@user.works.length).must_equal 1
+
+      @user.works << works(:book)
+      expect(@user.works.length).must_equal 2
+
+      @user.works << works(:movie)
+      expect(@user.works.length).must_equal 3
+    end
   end
 end
